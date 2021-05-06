32b9527014192482f9851d2b13905b2e8ce23874
Matt Blewett <matthewblewett@MacBook-Pro.local>
2021-05-06 10:56:11 -0400

    Added Othello env

--- toybox/envs/atari/__init__.py
+++ toybox/envs/atari/__init__.py
@@ -1,3 +1,4 @@
 from .breakout import BreakoutEnv
 from .amidar import AmidarEnv
 from .space_invaders import SpaceInvadersEnv
+from .othello import OthelloEnv
---
+++ toybox/envs/atari/othello.py
@@ -0,0 +1,13 @@
+from toybox.envs.atari.base import ToyboxBaseEnv
+from toybox.envs.atari.constants import *
+from toybox import Toybox, Input
+
+import json
+
+
+class OthelloEnv(ToyboxBaseEnv):
+    def __init__(self, frameskip=(0, 0), repeat_action_probability=0., grayscale=True, alpha=False):
+        super().__init__(Toybox('othello', grayscale),
+            frameskip, repeat_action_probability,
+            grayscale=grayscale,
+            alpha=alpha)
